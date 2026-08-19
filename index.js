c395056c2327a1cdedebe83be1d057ad0387ec02
Arcane <291515644+Arcane7pb@users.noreply.github.com>
2026-08-19 19:03:31 +0200

    Add the grid and header

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-function getStarted () {
-    for (i = 0; i < (16**2); i++){
+function getStarted (times) {
+    for (i = 0; i < (times**2); i++){
         
-        createSquare(16);
+        createSquare(times);
         
     };
 }
@@ -12,18 +12,30 @@ const container = document.querySelector(".container");
 
 function createSquare (times) {
     const square = document.createElement("div");
-    square.style.width = `${100/times}%`;
-    square.style.paddingTop = `${(100/times)}%`;
+    square.style.minWidth = `${100/times}%`;
+    square.style.paddingTop = `${(100/times-0.2)}%`;
+    square.style.minHeight = `${(100/times)}%`;
     square.setAttribute("class","grid");
     container.appendChild(square);
 }
+let button = document.querySelector("button")
 
-function createLine () {
-    const line = document.createElement("div");
-    container.appendChild(line);
-}
+button.addEventListener("click", () => {
+        let times = prompt("Insert how many boxes of width do you want","Max 100");
+        if (times <= 100 && times >= 0){
+            while (container.firstChild){
+                container.removeChild(container.lastChild);
+            };
+            button.textContent = `${times} x ${times}`;
+            return getStarted(times)
+        } else {
+            alert("Error number must be between 0 and 100");
+        }
+    });
+    
 
-getStarted()
 
-createLine()
-console.log(container.lastChild)
+
+getStarted(16)
+
+
