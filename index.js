40cf7f8b4788b562b4b600fb3368981c0c0a5b3d
Arcane <291515644+Arcane7pb@users.noreply.github.com>
2026-08-19 04:34:06 +0200

    Add create first row

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 function getStarted () {
     for (i = 0; i < 16; i++){
-
+        createSquare(16);
     };
 }
 
 const container = document.querySelector(".container");
 
-function createSquare () {
+function createSquare (times) {
     const square = document.createElement("div");
+    square.style.border = "1px solid";
+    square.style.width = `${100/times}%`;
+    square.style.paddingTop = `${(100/times)}%`;
+    square.style.maxHeight = `${100/times}%`
+    square.style.margin = "0"
+    square.setAttribute("class","grid")
+    container.appendChild(square);
 }
+
+getStarted()
