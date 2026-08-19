8b19bbc159cac559b2afdb0224685b746428333c
Arcane <291515644+Arcane7pb@users.noreply.github.com>
2026-08-19 04:03:21 +0200

    Create createSquare function

---
+++ index.js
@@ -0,0 +1,11 @@
+function getStarted () {
+    for (i = 0; i < 16; i++){
+
+    };
+}
+
+const container = document.querySelector(".container");
+
+function createSquare () {
+    const square = document.createElement("div");
+}
