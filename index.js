1df41a7461ecb6a654739a88d9439af94eb15402
Arcane <291515644+Arcane7pb@users.noreply.github.com>
2026-08-19 05:01:57 +0200

    Try some things

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
 function getStarted () {
-    for (i = 0; i < 16; i++){
+    for (i = 0; i < (16**2); i++){
+        
         createSquare(16);
+        
     };
 }
+
+//pensar en fer linies * la cuantitat de quadrats demanada 16*16 etxc
 
 const container = document.querySelector(".container");
 
 function createSquare (times) {
     const square = document.createElement("div");
-    square.style.border = "1px solid";
     square.style.width = `${100/times}%`;
     square.style.paddingTop = `${(100/times)}%`;
-    square.style.maxHeight = `${100/times}%`
-    square.style.margin = "0"
-    square.setAttribute("class","grid")
+    square.setAttribute("class","grid");
     container.appendChild(square);
 }
 
+function createLine () {
+    const line = document.createElement("div");
+    container.appendChild(line);
+}
+
 getStarted()
+
+createLine()
+console.log(container.lastChild)
